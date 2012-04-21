cddafd3de86f0885007e139afe933c39d2b573f5
Sdude6422 <sdcg6672@gmail.com>
2012-04-21 11:22:43 -0500

    Added new forms for project

---
+++ forms/accesorymod.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+	<?php ?>
+	</head>
+	<body>
+		<div id="body">
+	
+			<div id="productsection">
+				<div id="productpic">
+				<!--insert javascript product view? -->
+				</div>
+				<div id="productinfo">
+				</div>
+			</div>
+		</div>
+		
+	</body>
+</html>
---
+++ forms/productform.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+	<?php ?>
+	</head>
+	<body>
+		<div id="header">
+		</div>
+		<div id="body">
+			<div id="banner">
+			
+			</div>
+			<div id="nav">
+			</div>
+			<div id="productsection">
+				<div id="productpic">
+				<!--insert javascript product view? -->
+				</div>
+				<div id="productinfo"
+				</div>
+			</div>
+			<div id="recomendedaccess"
+			<!--insert javascript product view? -->
+			</div>
+		</div>
+		<div id="footer">
+		</div>
+	</body>
+</html>
