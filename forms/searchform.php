ac0fd22ed10bdeca55c2ea0c7ae1e2bf9dddff83
Sdude6422 <sdcg6672@gmail.com>
2012-04-21 11:23:18 -0500

    another forms/searchform.php

---
+++ forms/searchform.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+	<?php ?>
+	</head>
+	<body>
+		<div id="header">
+		</div>
+		<div id="body">
+			<div id="banner">
+				<div id="search">
+				<form>
+				<input type="text" name="Searchbox" /> <input type="submit" value="Search"/>		
+				</form>
+				</div>
+			</div>
+			<div id="nav">
+			</div>
+			<div id="productview">
+		
+			<!--insert javascript for product list in large icon list  view -->
+			</div>
+		</div>
+		<div id="footer">
+		</div>
+	</body>
+</html>
